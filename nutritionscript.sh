402535897172b7e4f299fe9dac225f470db64866
daniellis-wustl <daniel@daniels-mbp-2.attlocal.net>
2022-03-30 11:29:06 -0500

    updated bash script

--- nutritionscript.sh
+++ nutritionscript.sh
@@ -3,8 +3,8 @@
 git checkout main
 cd src
 cd NutritionScraper
-javac NutritionScraper.java
-java NutritionScraper.java 
+mvn clean compile assembly:single
+java -cp target/project-nutritionscraper-0.0.1-SNAPSHOT-jar-with-dependencies.jar NutritionScraper.main.java.NutritionScraper $1
 cd ..
 cd ..
 git checkout GautamiAndEfimia
