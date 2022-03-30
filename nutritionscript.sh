e7762fc48299f1f5ce688ae97df21e4ce33236bd
daniellis-wustl <daniel@daniels-mbp-2.attlocal.net>
2022-03-30 11:40:47 -0500

    bash updates

--- nutritionscript.sh
+++ nutritionscript.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 git checkout main
-cd src
 mvn clean compile assembly:single
 java -cp target/project-nutritionscraper-0.0.1-SNAPSHOT-jar-with-dependencies.jar NutritionScraper.main.java.NutritionScraper $1
 cd ..
