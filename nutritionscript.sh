52a05455ec63bc7922060aa3bee40770440ad980
Efimia Karageorgiou <efimiakarageorgiou@efimias-air.dhcp.wustl.edu>
2022-03-29 19:20:24 -0500

    in progress

--- nutritionscript.sh
+++ nutritionscript.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
 git checkout main
-cd resources 
-TEXTFILE=$(cat foodList.txt)
-cd ..
 cd src
 cd NutritionScraper
 javac NutritionScraper.java
 java NutritionScraper.java 
-read TEXTFILE
 cd ..
 cd ..
 git checkout GautamiAndEfimia
