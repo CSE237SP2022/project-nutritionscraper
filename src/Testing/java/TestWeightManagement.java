e04e4e3b116de9f98634bb3c9be27f66b1aadf14
Constance Li <consl414@gmail.com>
2022-04-20 14:01:53 -0500

    add test files

---
+++ src/Testing/java/TestWeightManagement.java
@@ -0,0 +1,14 @@
+package Testing.java;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestWeightManagement {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
